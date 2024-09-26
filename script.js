7ebc7cedb8e1afb4ffcec4d78179e388fc0f38d2
WLopesF <williamlopesfreitas789@gmail.com>
2024-09-26 11:25:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function solicitarDados() {
 
+    // Solicita o nome completo do usuário
+    let nomeCompleto = prompt("Olá, qual o seu nome completo?");
+
+    // Solicita a idade em anos do usuário
+    let idadeAnos = prompt("Qual a sua idade?");
+
+    // Solicita um hobbie do usuário
+    let hobbie = prompt("Cite algo que você gosta de fazer.")
+
+    // Exibe um comentário
+    alert("Olá, " + nomeCompleto + "! Você tem " + idadeAnos + " anos." + " Que bom que você gosta de " + hobbie + ".");
+}
