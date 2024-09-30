c2b8a2db98f5c134e59a45d9bdc09a022d24cb3e
WLopesF <williamlopesfreitas789@gmail.com>
2024-09-30 11:55:48 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ function questoesHTML() {
     //Nome do usuário
     let nomeCompleto = prompt("Para começo, qual seu nome?");
 
+    if(nomeCompleto = "") {
+    alert ("Insira seu nome.");
+    return false
+    
+
     //Tempo de aprendizado
     let tempoAprendizado = prompt("Há quanto tempo você começou a aprender HTML?");
 
+    if(tempoAprendizado) {
+    
 }
