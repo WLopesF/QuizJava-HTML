626669350b2a0ab056aa4e0585632b508c86d3c4
WLopesF <williamlopesfreitas789@gmail.com>
2024-10-03 11:55:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,61 @@
-function solicitarDados() {
+function questoesHTML() {
 
-    // Solicita o nome completo do usuário
-    let nomeCompleto = prompt("Olá, qual o seu nome completo?");
+    //Nome do usuário
+    let nomeCompleto = prompt("Para começo, qual seu nome?");
 
-    // Solicita a idade em anos do usuário
-    let idadeAnos = prompt("Qual a sua idade?");
+    if (nomeCompleto === "") {
+        alert("Insira seu nome.");
 
-    // Solicita um hobbie do usuário
-    let hobbie = prompt("Cite algo que você gosta de fazer.")
 
-    // Exibe um comentário
-    alert("Olá, " + nomeCompleto + "! Você tem " + idadeAnos + " anos." + " Que bom que você gosta de " + hobbie + ".");
+    }
+
+    //Tempo de aprendizado
+    let tempoAprendizado = prompt("Há quanto tempo você começou a aprender HTML?");
+
+    if (tempoAprendizado <= 0 || tempoAprendizado === "") {
+        alert("Insira um tempo válido");
+
+    }
+
+    if (tempoAprendizado === "") {
+        alert("Insira o seu tempo");
+    }
+
+    //Questões 
+    let tagFechamento = prompt("Qual caractere é utilizado para fechar uma tag?");
+
+    if (tagFechamento === "/") {
+        alert("Correto! Parabéns.");
+        let acerto = 1;
+        const acertos = 1;
+
+    } else {
+        alert("Errado! Que pena.");
+
+    }
+
+    let mainEbody = prompt("Qual a diferença entre a tag <main> e <body>?");
+
+    if (mainEbody === "Prioridade" || "Tamanho") {
+        mainEbody === ("Body é todo o conteúdo da página, e main é o conteúdo principal")
+        alert("Correto! Parabéns");
+        let acerto = 1;
+        const acertos = +1;
+
+    } else if (mainEbody === "Nenhuma")
+        alert("Errado! Que pena.");
+   
+
+    let estrutura = prompt("Desafio: Qual a estrutura de prioridade de tags , de <body> até <footer> na criação de um site?");
+
+    if (estrutura === "body, header, nav, main, section, aside e footer") {
+        alert("Correto! Parabéns. Essa foi difícil.");
+        let acerto = 1;
+        const acertos = +1;
+           alert("Obrigado por completar o quiz, " + nomeCompleto + ",para " + tempoAprendizado + " você está indo bem! Você acertou " + acertos + ".");
+    } else {
+        alert("Errado! Que pena.");
+    }
+
 }
+
