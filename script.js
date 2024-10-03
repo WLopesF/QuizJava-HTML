d9c756c0dbc8cba1770e87d769729385cfbdd0d6
WLopesF <williamlopesfreitas789@gmail.com>
2024-10-03 11:54:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,14 @@
-function questoesHTML() {
+function solicitarDados() {
 
-    //Nome do usuário
-    let nomeCompleto = prompt("Para começo, qual seu nome?");
+    // Solicita o nome completo do usuário
+    let nomeCompleto = prompt("Olá, qual o seu nome completo?");
 
-    if(nomeCompleto = "") {
-    alert ("Insira seu nome.");
-    return false
-    
-    //Tempo de aprendizado
-    let tempoAprendizado = prompt("Há quanto tempo você começou a aprender HTML?");
+    // Solicita a idade em anos do usuário
+    let idadeAnos = prompt("Qual a sua idade?");
 
-    if(tempoAprendizado = 0) {
-    alert ("Insira um tempo válido");
+    // Solicita um hobbie do usuário
+    let hobbie = prompt("Cite algo que você gosta de fazer.")
 
-    if(tempoAprendizado = "") {
-    alert ("Insira o seu tempo");
-
-    //Questões 
-    let tagFechamento = prompt("Qual caractere é utilizado para fechar uma tag?");
-   
-    if(tagFechanento = "/") {
-    alert ("Correto! Parabéns.");
-   
-    } else {
-    alert ("Errado! Que pena.");
-
-    let mainEbody = prompt("Qual a diferença entre a tag <main> e <body>?);
-
-    if(mainEbody = "Específidade"){
-    alert ("Correto! Parabéns");
-
-    } if(mainEbody = "Body é todo o conteúdo da página, e main é o conteúdo principal") {
-    alert ("Correto! Parabéns");
-
-    } if(mainEbody = "Nenhuma") {
-    alert (Errado! Que pena");
-
-    } else {
-    alert ("Errado! Que pena.");
-
-    
-
-    //Fim de quiz
-    alert ("Obrigado por completar o quiz + "nomeCompleto" + ,para "tempoAprendizado" você 
-   
+    // Exibe um comentário
+    alert("Olá, " + nomeCompleto + "! Você tem " + idadeAnos + " anos." + " Que bom que você gosta de " + hobbie + ".");
 }
